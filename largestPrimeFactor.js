b99f0933aa7145d0ce3373d5b5866684eb4ab104
Tyler <alexithemia@gmail.com>
2019-01-08 10:04:07 -1000

    Passing

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,27 @@
-exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+exports.largestPrimeFactor = function (n) {
+  var primeNumber = 2;
+  while (n > 2) {
+    if (n % primeNumber === 0) {
+      n = n / primeNumber;
+    } else (
+      primeNumber++
+    )
+  }
+  // function isPrime(num) {
+  //   var i = 2;
+  //   while (i < num) {
+  //     if (num % i === 0) {
+  //       return false;
+  //     }
+  //     i++;
+  //   }
+  //   return true;
+  // }
 
+  // for (var i = 2; i < n; i++) {
+  //   if (n % i === 0 && isPrime(i)) {
+  //     primeNumber = i;
+  //   }
+  // }
   return primeNumber;
 };
