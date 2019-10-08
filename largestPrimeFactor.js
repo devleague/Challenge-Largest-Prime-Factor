75744c9e947b576f818d4e39b1bb3a2026c401d7
SURYANSH SINGH RATHORE <rathoresuryansh196@gmail.com>
2019-10-08 16:21:25 +0530

    Update largestPrimeFactor.js

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,24 @@
 exports.largestPrimeFactor = function(n) {
-  let primeNumber = 0;
-  // do your work here
-
+  let primeNumber = -1;
+  var i,j;
+  
+  while(n%2==0)
+  { 
+       primeNumber = 2; 
+       n>>=1;  
+  } 
+  
+  for (int i = 3; i <= sqrt(n); i += 2)
+  { 
+        while(n%i==0)
+        { 
+            primeNumber=i; 
+            n=n/i; 
+        } 
+  } 
+  
+  if(n>2) 
+     primeNumber=n;
+  
   return primeNumber;
 };
